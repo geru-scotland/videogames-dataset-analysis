702e5b700f581e860f4b688c363010db79e174c4
geru-scotland <aingeru.scotland@gmail.com>
2023-12-31 13:32:33 +0100

    Docstrings

--- game_data_analyzer.py
+++ game_data_analyzer.py
@@ -1,10 +1,28 @@
 import os
 import pandas as pd
 import requests
+"""
+Script de Análisis de Datos de Videojuegos (GameDataAnalyzer)
 
+Este script utiliza la API de RAWG para recopilar y analizar datos sobre videojuegos. 
+Permite la extracción, procesamiento y almacenamiento de datos sobre géneros de videojuegos, 
+calificaciones, y tendencias a lo largo del tiempo. Los datos se procesan y convierten a
+formato ARFF para su uso en análisis de machine learning.
+
+Autor: Aingeru García
+GitHub: https://github.com/geru-scotland
+Repositorio del Proyecto: https://github.com/geru-scotland/videogames-dataset-analysis
+
+Requisitos:
+- Python 3
+- pandas, requests
+"""
 
 class GameDataAnalyzer:
+    """Clase para analizar datos de videojuegos usando la API de RAWG.
+    Se encarga de recopilar, procesar y convertir datos en varios formatos."""
     def __init__(self, api_key):
+        """Inicializa la instancia de GameDataAnalyzer con la clave API y configura los parámetros iniciales."""
         self.api_key = api_key
         self.url = 'https://api.rawg.io/api/games'
         self.params = {
@@ -15,6 +33,7 @@ class GameDataAnalyzer:
         self.create_directories()
 
     def create_directories(self):
+        """Crea directorios necesarios para almacenar los datos si aún no existen."""
         if not os.path.exists('data'):
             os.makedirs('data')
 
@@ -22,6 +41,7 @@ class GameDataAnalyzer:
             os.makedirs('data/arff')
 
     def fetch_data(self):
+        """Recopila datos de juegos de la API de RAWG y los almacena en un archivo CSV."""
         games_data = pd.DataFrame(columns=['name', 'genre', 'released', 'rating'])
         while True:
             response = requests.get(self.url, params=self.params)
@@ -46,6 +66,7 @@ class GameDataAnalyzer:
         games_data.to_csv('data/games_data_with_ratings.csv', index=False)
 
     def process_data(self):
+        """Procesa los datos recopilados, calculando estadísticas como el rating promedio y clasifica los géneros de juegos."""
         games_data = pd.read_csv('data/games_data_with_ratings.csv')
         games_data['released'] = pd.to_datetime(games_data['released'], errors='coerce')
         games_data['year'] = games_data['released'].dt.year
@@ -63,6 +84,7 @@ class GameDataAnalyzer:
         print("Results saved: 'data/average_ratings_count_success_by_genre_and_year.csv'")
 
     def convert_csv_to_arff(self, csv_file, arff_file):
+        """Convierte un archivo CSV a formato ARFF para análisis en aplicaciones de machine learning."""
         data = pd.read_csv(csv_file)
 
         with open(arff_file, 'w') as f:
